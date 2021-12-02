3022fab25a1166ad780160be25323447f3b5e9cc
Arkadiusz Żmudzin <arek@moodup.team>
2021-12-02 21:03:58 +0100

    Update solution to solve second step

--- aoc1-sonar-sweep/src/main.rs
+++ aoc1-sonar-sweep/src/main.rs
@@ -6,15 +6,24 @@ fn main() -> Result<(), std::io::Error> {
         .split("\n")
         .filter_map(|line| line.parse().ok())
         .collect();
-    let increases = count_depth_increases(&measurements);
-    println!("{}", increases);
+    println!("{}", count_depth_increases(&measurements, 1));
+    println!("{}", count_depth_increases(&measurements, 3));
     Ok(())
 }
 
-fn count_depth_increases(measurements: &Vec<usize>) -> usize {
+fn count_depth_increases(measurements: &Vec<usize>, window_size: usize) -> usize {
     measurements
+        .windows(window_size)
+        .map(|window| window.into_iter().sum())
+        .collect::<Vec<usize>>()
         .windows(2)
-        .filter_map(|window| if window[0] < window[1] { Some(()) } else { None } )
+        .filter_map(|window| {
+            if window[0] < window[1] {
+                Some(())
+            } else {
+                None
+            }
+        })
         .count()
 }
 
@@ -25,12 +34,18 @@ mod tests {
     #[test]
     fn basic_example() {
         let input = vec![199, 200, 201];
-        assert_eq!(count_depth_increases(&input), 2);
+        assert_eq!(count_depth_increases(&input, 1), 2);
     }
 
     #[test]
     fn example_from_website() {
         let input = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
-        assert_eq!(count_depth_increases(&input), 7)
+        assert_eq!(count_depth_increases(&input, 1), 7)
+    }
+
+    #[test]
+    fn test_sliding_window() {
+        let input = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+        assert_eq!(count_depth_increases(&input, 3), 5)
     }
 }
